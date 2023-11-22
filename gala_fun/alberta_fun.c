1e185d659047ee25000c6f880e930e25c6446b29
auzinska <auzinska@gmail.com>
2023-11-22 08:50:14 +0200

    Class_

--- gala_fun/alberta_fun.c
+++ gala_fun/alberta_fun.c
@@ -5,13 +5,13 @@ double mans_sinuss(double x){
     double a, S;
     int k = 0;
 
-    a = (pow(-1,k)*pow(x,2*k+1))/pow(2,2*k+1); 
+    a = (pow(x,k))/k; 
     S = a;
     printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
 
     while (k < 500){
         k++;
-        a = a*(-1)*pow(x,2)/((16*pow(k,2)+8*k));
+        a = a*(pow(x,k))/k;
         S = S + a;
         printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
     }
@@ -21,13 +21,11 @@ double mans_sinuss(double x){
 int main(){
 
     double x = 5,y,yy;
-    y = sin(x/2);
-
-
+    y = sin(exp(x));
 
     yy = mans_sinuss(x);
-    printf("lietojamā funkcija- y=mans_sinuss(%.2f)=%.2f\n",x,yy);
-    printf("Standarta funkcija- Y=sin(%.2f)=%.2f\n",x,y);
+    printf("lietojamā funkcija- y=mana_fun(%.2f)=%.2f\n",x,yy);
+    printf("Standarta funkcija- Y=exp(x)(%.2f)=%.2f\n",x,y);
     return 0;
 
 }
